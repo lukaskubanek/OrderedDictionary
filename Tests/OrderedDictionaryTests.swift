969f771eb24abba9c6f4fd922b98717569a3c8e9
Lukas Kubanek <lukas.kubanek@me.com>
2015-08-30 00:22:41 +0200

    Completed tests

--- Tests/OrderedDictionaryTests.swift
+++ Tests/OrderedDictionaryTests.swift
@@ -22,7 +22,9 @@ class OrderedDictionaryTests: XCTestCase {
         ]
     }
     
-    func testInitialization() {
+    // MARK: - Content
+    
+    func testInitializedContent() {
         expect(self.orderedDictionary.count) == 3
         
         expect(self.orderedDictionary["A"]) == 1
@@ -34,6 +36,74 @@ class OrderedDictionaryTests: XCTestCase {
         expect(self.orderedDictionary["C"]) == 3
         expect(self.orderedDictionary.indexForKey("C")) == 2
     }
+    
+    func testElementsGenerator() {
+        for entry in self.orderedDictionary.enumerate() {
+            expect(self.orderedDictionary[entry.index].0) == entry.element.0
+            expect(self.orderedDictionary[entry.index].1) == entry.element.1
+        }
+    }
+    
+    // MARK: - Key-based Modifications
+    
+    func testKeyBasedSubscript() {
+        self.orderedDictionary["A"] = 5
+        self.orderedDictionary["D"] = 10
+        self.orderedDictionary["B"] = nil
+        
+        expect(self.orderedDictionary.count) == 3
+        
+        expect(self.orderedDictionary["A"]) == 5
+        expect(self.orderedDictionary.indexForKey("A")) == 0
+        
+        expect(self.orderedDictionary["B"]).to(beNil())
+        expect(self.orderedDictionary.indexForKey("B")).to(beNil())
+        
+        expect(self.orderedDictionary["C"]) == 3
+        expect(self.orderedDictionary.indexForKey("C")) == 1
+        
+        expect(self.orderedDictionary["D"]) == 10
+        expect(self.orderedDictionary.indexForKey("D")) == 2
+    }
+    
+    // MARK: - Index-based Modifications
+    
+    func testIndexBasedSubscriptForRetrievingValues() {
+        let elementAtIndex0 = self.orderedDictionary[0]
+        expect(elementAtIndex0.0) == "A"
+        expect(elementAtIndex0.1) == 1
+        
+        let elementAtIndex1 = self.orderedDictionary[1]
+        expect(elementAtIndex1.0) == "B"
+        expect(elementAtIndex1.1) == 2
+        
+        let elementAtIndex2 = self.orderedDictionary[2]
+        expect(elementAtIndex2.0) == "C"
+        expect(elementAtIndex2.1) == 3
+    }
+    
+    func testIndexBasedSubscriptForSettingValues() {
+        self.orderedDictionary[0] = ("F", 10)
+        self.orderedDictionary[1] = ("B", 5)
+        
+        let elementAtIndex0 = self.orderedDictionary[0]
+        expect(elementAtIndex0.0) == "F"
+        expect(elementAtIndex0.1) == 10
+        
+        let elementAtIndex1 = self.orderedDictionary[1]
+        expect(elementAtIndex1.0) == "B"
+        expect(elementAtIndex1.1) == 5
+        
+        let elementAtIndex2 = self.orderedDictionary[2]
+        expect(elementAtIndex2.0) == "C"
+        expect(elementAtIndex2.1) == 3
+    }
+    
+    func testRetrievingElementAtNonExistentIndex() {
+        expect(self.orderedDictionary.elementAtIndex(10)).to(beNil())
+    }
+    
+    // MARK: - Removal
     
     func testRemoveAll() {
         self.orderedDictionary.removeAll()
@@ -81,67 +151,7 @@ class OrderedDictionaryTests: XCTestCase {
         expect(elementAtIndex0.1) == 3
     }
     
-    func testRetrievingElementAtNonExistentIndex() {
-        expect(self.orderedDictionary.elementAtIndex(10)).to(beNil())
-    }
-    
-    func testGenerator() {
-        for entry in self.orderedDictionary.enumerate() {
-            expect(self.orderedDictionary[entry.index].0) == entry.element.0
-            expect(self.orderedDictionary[entry.index].1) == entry.element.1
-        }
-    }
-    
-    func testKeyBasedSubscript() {
-        self.orderedDictionary["A"] = 5
-        self.orderedDictionary["D"] = 10
-        self.orderedDictionary["B"] = nil
-        
-        expect(self.orderedDictionary.count) == 3
-        
-        expect(self.orderedDictionary["A"]) == 5
-        expect(self.orderedDictionary.indexForKey("A")) == 0
-        
-        expect(self.orderedDictionary["B"]).to(beNil())
-        expect(self.orderedDictionary.indexForKey("B")).to(beNil())
-        
-        expect(self.orderedDictionary["C"]) == 3
-        expect(self.orderedDictionary.indexForKey("C")) == 1
-        
-        expect(self.orderedDictionary["D"]) == 10
-        expect(self.orderedDictionary.indexForKey("D")) == 2
-    }
-    
-    func testIndexBasedSubscriptForRetrievingValues() {
-        let elementAtIndex0 = self.orderedDictionary[0]
-        expect(elementAtIndex0.0) == "A"
-        expect(elementAtIndex0.1) == 1
-        
-        let elementAtIndex1 = self.orderedDictionary[1]
-        expect(elementAtIndex1.0) == "B"
-        expect(elementAtIndex1.1) == 2
-        
-        let elementAtIndex2 = self.orderedDictionary[2]
-        expect(elementAtIndex2.0) == "C"
-        expect(elementAtIndex2.1) == 3
-    }
-    
-    func testIndexBasedSubscriptForSettingValues() {
-        self.orderedDictionary[0] = ("F", 10)
-        self.orderedDictionary[1] = ("B", 5)
-        
-        let elementAtIndex0 = self.orderedDictionary[0]
-        expect(elementAtIndex0.0) == "F"
-        expect(elementAtIndex0.1) == 10
-        
-        let elementAtIndex1 = self.orderedDictionary[1]
-        expect(elementAtIndex1.0) == "B"
-        expect(elementAtIndex1.1) == 5
-        
-        let elementAtIndex2 = self.orderedDictionary[2]
-        expect(elementAtIndex2.0) == "C"
-        expect(elementAtIndex2.1) == 3
-    }
+    // MARK: - Description
     
     func testDescription() {
         expect(self.orderedDictionary.description) == "[A: 1, B: 2, C: 3]"
