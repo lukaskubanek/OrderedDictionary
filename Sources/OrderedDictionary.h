4d45fb22c81dde3db6aede15fd032e046003e612
Lukas Kubanek <lukas.kubanek@me.com>
2015-08-29 20:35:01 +0200

    Initial commit

---
+++ Sources/OrderedDictionary.h
@@ -0,0 +1,12 @@
+//
+//  OrderedDictionary.h
+//  OrderedDictionary
+//
+//  Created by Lukas Kubanek on 29/08/15.
+//  Copyright © 2015 Lukas Kubanek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double OrderedDictionaryVersionNumber;
+FOUNDATION_EXPORT const unsigned char OrderedDictionaryVersionString[];
