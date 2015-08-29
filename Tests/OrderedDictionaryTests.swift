41690628d0d67d04919dbc2ce14236bc970d8892
Lukas Kubanek <lukas.kubanek@me.com>
2015-08-29 23:30:45 +0200

    Added empty test target

---
+++ Tests/OrderedDictionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrderedDictionaryTests.swift
+//  OrderedDictionaryTests
+//
+//  Created by Lukas Kubanek on 29/08/15.
+//  Copyright © 2015 Lukas Kubanek. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import OrderedDictionary
+
+class OrderedDictionaryTests: XCTestCase {
+    
+    
+    
+}
