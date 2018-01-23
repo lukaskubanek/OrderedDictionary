28cd25d536bb9339c8f63c24a1f0c113620c2e0d
Lukas Kubanek <lukas.kubanek@me.com>
2018-01-23 15:43:56 +0100

    Added support for moving elements to new index

--- Sources/OrderedDictionary.swift
+++ Sources/OrderedDictionary.swift
@@ -397,7 +397,40 @@ public struct OrderedDictionary<Key: Hashable, Value>: BidirectionalCollection {
     }
     
     // ======================================================= //
-    // MARK: - Sorting
+    // MARK: - Moving Elements
+    // ======================================================= //
+    
+    /// Moves an existing key-value pair specified by the given key to the new index by removing it
+    /// from its original index first and inserting it at the new index. If the movement is
+    /// actually performed, the previous index of the key-value pair is returned. Otherwise, `nil`
+    /// is returned.
+    ///
+    /// - Parameter key: The key specifying the key-value pair to move.
+    /// - Parameter newIndex: The new index the key-value pair should be moved to.
+    /// - Returns: The previous index of the key-value pair if it was sucessfully moved.
+    @discardableResult
+    public mutating func moveElement(forKey key: Key, to newIndex: Index) -> Index? {
+        // Load the previous index and return nil if the index is not found.
+        guard let previousIndex = index(forKey: key) else { return nil }
+        
+        // If the previous and new indices match, threat it as if the movement was already
+        // performed.
+        guard previousIndex != newIndex else { return previousIndex }
+        
+        // Remove the value for the key at its original index.
+        let value = removeValue(forKey: key)!
+        
+        // Validate the new index.
+        precondition(canInsert(at: newIndex), "Cannot move to invalid index in OrderedDictionary")
+
+        // Insert the element at the new index.
+        insert((key: key, value: value), at: newIndex)
+        
+        return previousIndex
+    }
+    
+    // ======================================================= //
+    // MARK: - Sorting Elements
     // ======================================================= //
     
     /// Sorts the ordered dictionary in place, using the given predicate as the comparison between
