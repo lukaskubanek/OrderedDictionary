33fa9622ce43521ed06074b7c91d65ff7ebb872a
Lukas Kubanek <lukas.kubanek@me.com>
2019-07-30 15:40:31 +0200

    Removed irrelevant part of a playground
    Signed-off-by: Lukas Kubanek <lukas.kubanek@me.com>

--- Playgrounds/OrderedDictionary+Codable.playground/Contents.swift
+++ Playgrounds/OrderedDictionary+Codable.playground/Contents.swift
@@ -31,19 +31,3 @@ let plistDecoder = PropertyListDecoder()
 let orderedDictionary3 = try! plistDecoder.decode(OrderedDictionary<String, Int>.self, from: plistData)
 
 orderedDictionary1 == orderedDictionary3
-
-// ======================================================= //
-// MARK: - Non-codable Type
-// ======================================================= //
-
-struct NonCodableType {
-    var string: String
-}
-
-let orderedDictionary4: OrderedDictionary<String, NonCodableType> = [
-    "A" : NonCodableType(string: "Foo"),
-    "B" : NonCodableType(string: "Bar"),
-    "C" : NonCodableType(string: "Baz")
-]
-
-//try? jsonEncoder.encode(x)
