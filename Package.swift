e0058ff43ca062d60b4d766ab5a1aab39fc35fd7
Lukas Kubanek <lukas.kubanek@me.com>
2020-05-21 15:46:32 +0200

    Updated manifest files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -22,6 +22,5 @@ let package = Package(
             dependencies: ["OrderedDictionary"],
             path: "Tests"
         )
-    ],
-    swiftLanguageVersions: [4]
+    ]
 )
