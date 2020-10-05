8a7247d9d443c4396fc4e56072c24d66a88e7bde
Lukas Kubanek <lukas.kubanek@me.com>
2020-10-05 12:14:39 +0200

    Made XCTUnwrap shim internal

--- Tests/OrderedDictionary/XCTUnwrapShim.swift
+++ Tests/OrderedDictionary/XCTUnwrapShim.swift
@@ -1,11 +1,11 @@
-//https://github.com/realm/SwiftLint/pull/2965
+// https://github.com/realm/SwiftLint/pull/2965
 // https://bugs.swift.org/browse/SR-11501
 #if compiler(<5.1) || (SWIFT_PACKAGE && os(macOS))
-private enum UnwrapError: Error {
+internal enum UnwrapError: Error {
     case missingValue
 }
 
-private func XCTUnwrap<T>(
+internal func XCTUnwrap<T>(
     _ expression: @autoclosure () throws -> T?,
     _ message: @autoclosure () -> String = ""
 ) throws -> T {
